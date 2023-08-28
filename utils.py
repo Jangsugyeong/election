27f7fc47bed3dcbd3e5fd9c1d67c95fb01fe4afb
Hardy <janghardy77@gmail.com>
2023-08-28 16:55:40 +0900

    update utils.py

--- utils.py
+++ utils.py
@@ -4,3 +4,35 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def save_excel(df):
+    '''
+    데이터 프레임을 엑셀로 저장
+    --------------------
+
+    parameters
+    --------------------
+    df : 엑셀로 저장할 데이터 프레임
+    '''
+    df.to_excel(f'{df}.xlsx', index = False, engine = 'openpyxl')
+
+
+def load_code(sgids, sgtypecodes, sgnames, sgvotedates):
+    '''
+    선거 코드 정보 불러오기
+    '''
+    # 빈 리스트 생성
+    sgid_list, sgtypecode_list, sgname_list, sgvotedate_list = []
+    
+    for sgid, sgtypecode, sgname, sgvotedate in zip(sgids, sgtypecodes, sgnames, sgvotedates):
+        sgid_list.append(sgid.get_text())
+        sgtypecode_list.append(sgtypecode.get_text())
+        sgname_list.append(sgname.get_text())
+        sgvotedate_list.append(sgvotedate.get_text())
+
+    df = pd.DataFrame({
+        "선거 ID" : sgid_list,
+        "선거종류코드" : sgtypecode_list,
+        "선거명" : sgname_list,
+    })
+
+    return df
